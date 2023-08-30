5ae53dc91f952049cbf369a1ef972e6b4d9dd934
eky <shadowofs@gmail.com>
2023-08-30 17:57:23 +0800

    unittest

--- go/pkg/scaler/scaler_test.go
+++ go/pkg/scaler/scaler_test.go
@@ -13,59 +13,59 @@ import (
 	"time"
 )
 
-//var _ = Describe("Assign instance", Ordered, func() {
-//	var cfg *config.Config
-//	var scaler scaler2.Scaler
-//	var createDurationMs = 10
-//
-//	BeforeAll(func() {
-//		cfg = &config.DefaultConfig
-//		metaData := &model.Meta{
-//			Meta: pb.Meta{
-//				Key:           "test",
-//				Runtime:       "go",
-//				TimeoutInSecs: 60,
-//				MemoryInMb:    128,
-//			},
-//		}
-//		scaler = scaler2.NewWithClient(metaData, cfg, platform_client.NewFakeClient(createDurationMs))
-//	})
-//
-//	When("no idle instance", func() {
-//		It("should create idle instance", func(ctx context.Context) {
-//			stats := scaler.Stats()
-//			Expect(stats.TotalIdleInstance).To(Equal(0))
-//
-//			req := &pb.AssignRequest{
-//				RequestId: uuid.New().String(),
-//			}
-//			ins, err := scaler.Assign(ctx, req)
-//			Expect(err).To(BeNil())
-//			Expect(ins).NotTo(BeNil())
-//			time.Sleep(time.Millisecond * time.Duration(createDurationMs))
-//			stats = scaler.Stats()
-//			Expect(stats.TotalIdleInstance).To(Equal(cfg.BufferSize))
-//		}, SpecTimeout(time.Second))
-//	})
-//
-//	When("idle instance available", func() {
-//		BeforeEach(func() {
-//			stats := scaler.Stats()
-//			Expect(stats.TotalIdleInstance).To(Equal(cfg.BufferSize))
-//		})
-//
-//		It("should use idle instance", func(ctx context.Context) {
-//			req := &pb.AssignRequest{
-//				RequestId: uuid.New().String(),
-//			}
-//			ins, err := scaler.Assign(ctx, req)
-//			Expect(err).To(BeNil())
-//			Expect(ins).NotTo(BeNil())
-//			stats := scaler.Stats()
-//			Expect(stats.TotalIdleInstance).To(Equal(cfg.BufferSize - 1))
-//		}, SpecTimeout(time.Second))
-//	})
-//})
+var _ = Describe("Assign instance", Ordered, func() {
+	var cfg *config.Config
+	var scaler scaler2.Scaler
+	var createDurationMs = 10
+
+	BeforeAll(func() {
+		cfg = &config.DefaultConfig
+		metaData := &model.Meta{
+			Meta: pb.Meta{
+				Key:           "test",
+				Runtime:       "go",
+				TimeoutInSecs: 60,
+				MemoryInMb:    128,
+			},
+		}
+		scaler = scaler2.NewWithClient(metaData, cfg, platform_client.NewFakeClient(createDurationMs))
+	})
+
+	When("no idle instance", func() {
+		It("should create idle instance", func(ctx context.Context) {
+			stats := scaler.Stats()
+			Expect(stats.TotalIdleInstance).To(Equal(0))
+
+			req := &pb.AssignRequest{
+				RequestId: uuid.New().String(),
+			}
+			ins, err := scaler.Assign(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(ins).NotTo(BeNil())
+			time.Sleep(time.Millisecond * time.Duration(createDurationMs))
+			stats = scaler.Stats()
+			Expect(stats.TotalIdleInstance).To(Equal(cfg.BufferSize))
+		}, SpecTimeout(time.Second))
+	})
+
+	When("idle instance available", func() {
+		BeforeEach(func() {
+			stats := scaler.Stats()
+			Expect(stats.TotalIdleInstance).To(Equal(cfg.BufferSize))
+		})
+
+		It("should use idle instance", func(ctx context.Context) {
+			req := &pb.AssignRequest{
+				RequestId: uuid.New().String(),
+			}
+			ins, err := scaler.Assign(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(ins).NotTo(BeNil())
+			stats := scaler.Stats()
+			Expect(stats.TotalIdleInstance).To(Equal(cfg.BufferSize - 1))
+		}, SpecTimeout(time.Second))
+	})
+})
 
 var _ = Describe("Idle instance", func() {
 	var scaler scaler2.Scaler
